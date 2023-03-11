075eb295649cff009579b5670749e885862a8e29
Verusca silva <56118750+Vesilva33@users.noreply.github.com>
2023-03-11 23:45:50 +0000

    ok

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("enviar").addEventListener("click",validaformulario)
+
+function validaformulario(){
+  if (document.getElementById("nome").value != ""&&   
+  document.getElementById("email").value != ""){
+   alert("Prontinho! Você receberá as novidades por email.")
+}
+else{
+ alert("Por favor, preencha os campos nome e email!")
+}
+}
+
